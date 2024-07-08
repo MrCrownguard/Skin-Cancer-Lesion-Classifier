4ac40a5f03432cd9e093f4c99849b7f370a243cc
Laith <60152667+MrCrownguard@users.noreply.github.com>
2024-07-08 16:42:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,31 +13,16 @@ import os
 import matplotlib.image as mpimg
 from werkzeug.utils import secure_filename
 
-#Create an app object using the Flask class. 
 app = Flask(__name__)
 
 app.config['IMAGE_UPLOADS'] = 'images'
-#numbered_classes = dict(enumerate(class_names))
 
-#Load the trained model. (Pickle file)
 model = tf.keras.models.load_model('models/skin_model.keras')
 
-#Define the route to be home. 
-#The decorator below links the relative route of the URL to the function it is decorating.
-#Here, home function is with '/', our root directory. 
-#Running the app sends us to index.html.
-#Note that render_template means it looks for the file in the templates folder. 
-
-#use the route() decorator to tell Flask what URL should trigger our function.
 @app.route('/')
 def home():
     return render_template('index.html')
 
-#You can use the methods argument of the route() decorator to handle different HTTP methods.
-#GET: A GET message is send, and the server returns data
-#POST: Used to send HTML form data to the server.
-#Add Post method to the decorator to allow for form submission. 
-#Redirect to /predict page with the output
 class_names = ['Actinic keratoses and intraepithelial carcinoma / Bowens disease', 'basal cell carcinoma',
                'benign keratosis-like lesions', 'dermatofibroma ', 'melanoma ','melanocytic nevi', 'vascular lesions']
 @app.route('/predict',methods=["POST","GET"])
@@ -52,11 +37,6 @@ def predict():
     #prediction = model.predict(features)  # features Must be in the form [[a, b]]
     filename = secure_filename(image.filename)
     basedir = os.path.abspath(os.path.dirname(__file__))
-    #folder = os.path.join(basedir,app.config['IMAGE_UPLOADS'])
-    #for filename in os.listdir(folder):
-    #    file_path = os.path.join(folder, filename)
-    #    if os.path.isfile(file_path):
-    #        os.remove(file_path)
     image.save(os.path.join(basedir,app.config['IMAGE_UPLOADS'],filename))
     img = mpimg.imread(os.path.join(basedir,app.config['IMAGE_UPLOADS'],filename))
     res = cv2.resize(img, dsize=(32, 32), interpolation=cv2.INTER_CUBIC)
@@ -70,18 +50,7 @@ def predict():
     #return render_template("index.html",prediction_text='The skin abnormality is most likely {}'.format(class_names[highest]))
     return render_template("index.html",prediction_text='The model predicts this skin abnormality is {}'.format(class_names[highest]))
     
-#output = round(prediction[0], 2)
 
-    #return render_template('index.html', prediction_text='Percent with heart disease is {}'.format(img))
-
-
-#When the Python interpreter reads a source file, it first defines a few special variables. 
-#For now, we care about the __name__ variable.
-#If we execute our code in the main program, like in our case here, it assigns
-# __main__ as the name (__name__). 
-#So if we want to run our code right here, we can check if __name__ == __main__
-#if so, execute it here. clas
-#If we import this file (module) to another file then __name__ == app (which is the name of this python file).
 
 if __name__ == "__main__":
-    app.run(port=5031)
+    app.run(port=5000)
